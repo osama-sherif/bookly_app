8f6a5196b6ae0e28e8620a2226173d6ced0774a1
Osama sherif <Os3x120wx@gmail.com>
2025-04-30 19:17:29 +0300

    adding sliding animation to slash view

--- lib/Features/splash/presentation/views/widgets/splas_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splas_view_body.dart
@@ -1,8 +1,40 @@
+import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplasViewBody extends StatelessWidget {
+class SplasViewBody extends StatefulWidget {
   const SplasViewBody({super.key});
+
+  @override
+  State<SplasViewBody> createState() => _SplasViewBodyState();
+}
+
+class _SplasViewBodyState extends State<SplasViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController; // values from zero to one
+  late Animation<Offset>
+  slidingAnimation; // take the values from animation control and use it
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +44,7 @@ class SplasViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         SizedBox(height: 4),
-        const Text(
-          'Read free books',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
