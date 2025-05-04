350631bdf000be67428d82650ce09663b2ffabae
Osama sherif <Os3x120wx@gmail.com>
2025-05-04 18:54:58 +0300

    refactor navigation

--- lib/Features/splash/presentation/views/widgets/splas_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splas_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
-import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplasViewBody extends StatefulWidget {
   const SplasViewBody({super.key});
@@ -58,11 +57,7 @@ class _SplasViewBodyState extends State<SplasViewBody>
 
   void homeViewNavigation() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.fadeIn,
-        duration: kTranstionDuration,
-      );
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -13,7 +12,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
@@ -21,7 +21,6 @@ class BooklyApp extends StatelessWidget {
           ThemeData.dark().textTheme,
         ),
       ),
-      home: const SplashView(),
     );
   }
 }
