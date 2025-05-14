77df032383113cec0d16ddbca6415cd905bbe824
Osama sherif <Os3x120wx@gmail.com>
2025-05-14 19:40:59 +0300

    create home repo

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,6 @@
+// select the actions of feature put not how to do this actions
+// as the actions may be done with the diffrent ways
+abstract class HomeRepo {
+  fetchBestSellerBooks();
+  fetchFeaturedBooks();
+}
--- lib/Features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -16,6 +16,5 @@ class SearchResultListView extends StatelessWidget {
         );
       },
     );
-    ;
   }
 }
