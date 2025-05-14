612dfbc239f9c3e6957b7be45d20d7a3ed0920d9
Osama sherif <Os3x120wx@gmail.com>
2025-05-14 21:15:46 +0300

    create api service and rapper

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+// rapper class of the service
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+
+  final Dio dio;
+  ApiService(this.dio);
+
+  Future<Map<String, dynamic>> get({required String endPoints}) async {
+    var response = await dio.get('$_baseUrl$endPoints');
+    return response.data;
+  }
+}
