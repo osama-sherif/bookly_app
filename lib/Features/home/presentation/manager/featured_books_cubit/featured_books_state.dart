711a2b91b5eb409c20fe2b4637597db9d3c334f2
Osama sherif <Os3x120wx@gmail.com>
2025-05-20 18:22:39 +0300

    create featued books method

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -19,5 +19,5 @@ final class FeaturedBooksFailure extends FeaturedBooksState {
 final class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
 
-  const FeaturedBooksSuccess({required this.books});
+  const FeaturedBooksSuccess(this.books);
 }
